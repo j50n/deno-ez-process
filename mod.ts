79758802edd612d539b42d8237664b9e8abbf810
j50n <j50n@lilypepper.com>
2022-09-18 14:21:12 -0600

    starting to look at defining more complex use cases

--- mod.ts
+++ mod.ts
@@ -1,3 +1,13 @@
+import * as proc from "https://deno.land/x/proc@0.19.7/mod.ts"
+
 export function hello(): string {
   return "Hello. This is a placeholder.";
 }
+
+interface RunOptions {
+
+}
+
+export function<T> run(options?: Options, ...args: string[]): T {
+
+} 
---
+++ use-cases.md
@@ -0,0 +1,8 @@
+## Multiprocessing
+
+What is the simplest form for the following?
+
+- input is an iterator of something 
+- run one process per item
+- convert the output to something else
+- flatten the multiple runs into a single stream 
